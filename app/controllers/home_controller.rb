367f0693fbd34f4731a6499d9885f7c00076fc0b
knchst0704 <knchst0704@gmail.com>
2016-08-26 22:49:11 +0900

    :+1: Add latest article

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :basic_auth, only: [:index]
 
   def index
+    @latest_articles = Article.where(published: true).order(created_at: :DESC)
   end
 end

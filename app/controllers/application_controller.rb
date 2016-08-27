5125b8fecefb11729d79581996df265162516692
knchst0704 <knchst0704@gmail.com>
2016-08-28 06:21:02 +0900

    :recycle: Rm cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :basic_auth
-  before_action :set_cache
   before_action :set_footer_tags
 
   def basic_auth
     authenticate_or_request_with_http_basic do |user, pass|
       user == "admin" && "eropit333admin"
     end
-  end
-
-  def set_cache
-    expires_in 1.day, public: true, must_revalidate: true
   end
 
   def set_footer_tags

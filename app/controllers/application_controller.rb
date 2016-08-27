af18cf3b011b888bb190d9ee0ac3ae66772e2bf7
knchst0704 <knchst0704@gmail.com>
2016-08-28 04:27:18 +0900

    :+1: Add footer tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :basic_auth
   before_action :set_cache
+  before_action :set_footer_tags
 
   def basic_auth
     authenticate_or_request_with_http_basic do |user, pass|
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def set_cache
     expires_in 1.day, public: true, must_revalidate: true
+  end
+
+  def set_footer_tags
+    @footer_tags = ActsAsTaggableOn::Tag.most_used(6)
   end
 end

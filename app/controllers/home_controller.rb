757121c6fd33132032b18a16e64c2242e272a265
wakatsuki <wakatsuki@commonize.jp>
2016-08-28 01:56:22 +0900

    revert cache controll and change ordering of resources

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :basic_auth, only: [:index]
 
   def index
-    expires_in 2.hour
     @latest_articles = Article.latest.published
     @popular_articles = Article.in_day.popular(6).published
     @recommendations = Article.tagged_with(ActsAsTaggableOn::Tag.most_used, :any => true).take(4)
